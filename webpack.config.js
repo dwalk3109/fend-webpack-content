6a2d44cf00202ca91c9f0b097db04b7d6a57e350
Darryl Walker <dew02d@gmail.com>
2024-08-25 14:40:57 -0500

    updating configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,13 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/client/index.js',
     mode: 'none',
+    module: {
+        rules: [
+            {
+                test:'/\.js$/',
+                exclude:/node_modules/,
+                loader:'babel-loader'
+            }
+        ]
+    }
 }
